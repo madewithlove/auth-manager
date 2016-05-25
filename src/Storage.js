a7121863811949d3617d5fe06bbec30644fc1790
Maxime Fabre <ehtnam6@gmail.com>
2016-05-25 12:13:31 +0200

    Pass options to save

--- src/Storage.js
+++ src/Storage.js
@@ -4,8 +4,8 @@ export default {
     get(value) {
         return cookies.load(value);
     },
-    set(key, value) {
-        cookies.save(key, value);
+    set(key, value, options) {
+        cookies.save(key, value, options);
     },
     remove(key) {
         cookies.remove(key);
